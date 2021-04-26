889eb67a3d98f43ce2990dff0fc260bbf7d4cfbe
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2021-04-27 03:10:55 +0500

    sdfsd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 // import logo from './logo.svg';
 import logo from "./logo192.png";
+import doctorImage1 from "./static/images/doctor1.png"
+import doctorImage2 from "./static/images/doctor2.png"
+import blueBC from "./static/images/blueToPurple.jpg";
 import './App.css';
 import { LandingComp } from './Components/LandingComp';
 
 const data = [
   {
-    text : 'SHAKEEL hAIDER',
-    image: logo
+    text : `Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus.
+    Most people who fall sick with COVID-19 will experience mild to moderate symptoms and recover without special treatment.`,
+    image: doctorImage1,
+    bcImage : blueBC
   },
   {
-    text : 'SHAKEEL HAIDER 2',
-    image: logo
+    text : `The virus that causes COVID-19 is mainly transmitted through droplets generated when an infected person coughs, sneezes, or exhales. These droplets are too heavy to hang in the air, and quickly fall on floors or surfaces.
+    You can be infected by breathing in the virus if you are within close proximity of someone who has COVID-19, or by touching a contaminated surface and then your eyes, nose or mouth.`,
+    image: doctorImage2,
+    bcImage : blueBC
   },
   {
     text : 'SHAKEEL HAIDER 3',
-    image: logo
+    image: logo,
+    bcImage: null
   },
   {
     text : 'SHAKEEL HAIDER 4',
-    image: logo
+    image: logo,
+    bcImage: null
   },
 ]
 
@@ -27,7 +36,7 @@ function App() {
   return <div>
       {
         data.map(
-          (obj, ind) => <LandingComp TEXT={obj.text} IMAGE={obj.image} key={ind} />
+          (obj, ind) => <LandingComp TEXT={obj.text} IMAGE={obj.image} BCIMAGE={obj.bcImage} key={ind} />
         )
       }
   </div>
--- src/Components/LandingComp.js
+++ src/Components/LandingComp.js
@@ -1,18 +1,52 @@
 import React from 'react'
 
-export const LandingComp = ({TEXT, IMAGE}) => {
+export const LandingComp = ({TEXT, IMAGE, BCIMAGE, key}) => {
 
-    return <div style={{
-        backgroundColor : '#4f3c3a', height : '100vh', display : 'grid', justifyContent : 'center', alignContent : 'center'
-    }} >
+    let myTransform = { transform : 'rotate(0)'  }
+    let stylee = {}
+    if(key%2 === 0)
+    {
+        myTransform = { transform : 'rotate(180)' }
+    }
+    if(BCIMAGE)
+    {
+        stylee = {
+            backgroundImage : `url(${BCIMAGE})`,
+            backgroundPosition: 'center', /* Center the image */
+            backgroundRepeat: 'no-repeat', /* Do not repeat the image */
+            backgroundSize: 'cover', /* Resize the background image to cover the entire container */
+            myTransform,
+            height : '100vh', 
+            display : 'grid', 
+            justifyContent : 'center', 
+            alignContent: 'center'
+        }
+    }else
+    {
+        stylee = {
+            backgroundColor : '#4f3c3a',
+            height : '100vh', 
+            display : 'grid', 
+            justifyContent : 'center', 
+            alignContent: 'center'
+        }
+    }
+    
+    return <div style={stylee} >
 
     <div style={{
         display : 'flex',
-        flexDirection: 'row'
+        flexDirection: 'row',
+        width : '100%',
+        justifyContent : 'center',
+        alignItems: 'center'
     }} >
-        <span style={{ flexGrow : 1 }} > {TEXT} </span>
-        <div style={{ flexGrow : 1 }} >
-            <img src={IMAGE} alt="jkjk" />
+        {/* <div style={{ width , border : '1px solid white' }} >
+            <div style={{  border : '1px solid white' }} > {TEXT} </div>
+        </div> */}
+        <span style={{ width : '30%', textAlign : 'center', fontSize : 12, fontWeight : 'bold', color : 'whitesmoke' }} > {TEXT} </span>
+        <div style={{   }} >
+            <img style={{ width : '100%' }} src={IMAGE} alt="jkjk" />
         </div>
 
     </div>
