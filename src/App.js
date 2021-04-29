e52f0791cf88d5942a09351fc3c233fe66d04d64
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2021-04-29 13:24:48 +0500

    sdfsdf

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@
 import logo from "./logo192.png";
 import doctorImage1 from "./static/images/doctor1.png"
 import doctorImage2 from "./static/images/doctor2.png"
+import doctorImage3 from "./static/images/doctor3.png";
+import doctorImage4 from "./static/images/doctor4.png";
+
 import blueBC from "./static/images/blueToPurple.jpg";
+import frame2 from "./static/images/frame2.png";
+import frame3 from "./static/images/frame3.png";
+import frame4 from "./static/images/frame4.png";
 import './App.css';
 import { LandingComp } from './Components/LandingComp';
 
@@ -10,24 +16,26 @@ const data = [
   {
     text : `Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus.
     Most people who fall sick with COVID-19 will experience mild to moderate symptoms and recover without special treatment.`,
-    image: doctorImage1,
+    image: doctorImage2,
     bcImage : blueBC
   },
   {
     text : `The virus that causes COVID-19 is mainly transmitted through droplets generated when an infected person coughs, sneezes, or exhales. These droplets are too heavy to hang in the air, and quickly fall on floors or surfaces.
     You can be infected by breathing in the virus if you are within close proximity of someone who has COVID-19, or by touching a contaminated surface and then your eyes, nose or mouth.`,
-    image: doctorImage2,
-    bcImage : blueBC
+    image: doctorImage1,
+    bcImage : frame2
   },
   {
-    text : 'SHAKEEL HAIDER 3',
-    image: logo,
-    bcImage: null
+    text : `Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus.
+    Most people who fall sick with COVID-19 will experience mild to moderate symptoms and recover without special treatment.`,
+    image: doctorImage3,
+    bcImage: frame3
   },
   {
-    text : 'SHAKEEL HAIDER 4',
-    image: logo,
-    bcImage: null
+    text : `Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus.
+    Most people who fall sick with COVID-19 will experience mild to moderate symptoms and recover without special treatment.`,
+    image: doctorImage4,
+    bcImage: frame4
   },
 ]
 
@@ -36,7 +44,7 @@ function App() {
   return <div>
       {
         data.map(
-          (obj, ind) => <LandingComp TEXT={obj.text} IMAGE={obj.image} BCIMAGE={obj.bcImage} key={ind} />
+          (obj, ind) => <LandingComp TEXT={obj.text} IMAGE={obj.image} BCIMAGE={obj.bcImage} INDEX={ind+1} key={ind} />
         )
       }
   </div>
--- src/Components/LandingComp.js
+++ src/Components/LandingComp.js
@@ -1,55 +1,65 @@
 import React from 'react'
 
-export const LandingComp = ({TEXT, IMAGE, BCIMAGE, key}) => {
+export const LandingComp = ({ TEXT, IMAGE, BCIMAGE, INDEX }) => {
 
-    let myTransform = { transform : 'rotate(0)'  }
-    let stylee = {}
-    if(key%2 === 0)
-    {
-        myTransform = { transform : 'rotate(180)' }
+    let myTransform = { transform: 'rotate(0)' }
+    let stylee = {
+        backgroundImage: `url(${BCIMAGE})`,
+        backgroundPosition: 'center', /* Center the image */
+        backgroundRepeat: 'no-repeat', /* Do not repeat the image */
+        backgroundSize: 'cover', /* Resize the background image to cover the entire container */
+        myTransform,
+        height: '100vh',
+        display: 'grid',
+        justifyContent: 'center',
+        alignContent: 'center'
     }
-    if(BCIMAGE)
-    {
-        stylee = {
-            backgroundImage : `url(${BCIMAGE})`,
-            backgroundPosition: 'center', /* Center the image */
-            backgroundRepeat: 'no-repeat', /* Do not repeat the image */
-            backgroundSize: 'cover', /* Resize the background image to cover the entire container */
-            myTransform,
-            height : '100vh', 
-            display : 'grid', 
-            justifyContent : 'center', 
-            alignContent: 'center'
-        }
-    }else
-    {
-        stylee = {
-            backgroundColor : '#4f3c3a',
-            height : '100vh', 
-            display : 'grid', 
-            justifyContent : 'center', 
-            alignContent: 'center'
-        }
-    }
-    
-    return <div style={stylee} >
 
-    <div style={{
-        display : 'flex',
-        flexDirection: 'row',
-        width : '100%',
-        justifyContent : 'center',
-        alignItems: 'center'
-    }} >
-        {/* <div style={{ width , border : '1px solid white' }} >
+    console.log(INDEX)
+    if (INDEX % 2 !== 0) {
+        return <div style={stylee} >
+
+
+            <div style={{
+                display: 'flex',
+                flexDirection: 'row',
+                width: '100%',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }} >
+                {/* <div style={{ width , border : '1px solid white' }} >
             <div style={{  border : '1px solid white' }} > {TEXT} </div>
         </div> */}
-        <span style={{ width : '30%', textAlign : 'center', fontSize : 12, fontWeight : 'bold', color : 'whitesmoke' }} > {TEXT} </span>
-        <div style={{   }} >
-            <img style={{ width : '100%' }} src={IMAGE} alt="jkjk" />
+                <span style={{ width: '30%', textAlign: 'center', fontSize: 12, fontWeight: 'bold', color: 'whitesmoke' }} > {TEXT} </span>
+                <div style={{}} >
+                    <img style={{ width: '100%' }} src={IMAGE} alt="jkjk" />
+                </div>
+
+            </div>
+
         </div>
+    }
+    else {
+        return <div style={stylee} >
 
-    </div>
 
-    </div>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'row',
+                width: '100%',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }} >
+                {/* <div style={{ width , border : '1px solid white' }} >
+            <div style={{  border : '1px solid white' }} > {TEXT} </div>
+        </div> */}
+                <div style={{}} >
+                    <img style={{ width: '100%' }} src={IMAGE} alt="jkjk" />
+                </div>
+                <span style={{ width: '30%', textAlign: 'center', fontSize: 12, fontWeight: 'bold', color: 'whitesmoke' }} > {TEXT} </span>
+
+            </div>
+
+        </div>
+    }
 }
